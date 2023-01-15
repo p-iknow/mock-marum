d3bde98788c5766e64d649848dfe56505bd84cd2
p-iknow <apricotsoul@gmail.com>
2023-01-15 18:46:36 +0900

    config: twin.macro

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
+// refer: https://github.com/ben-rogerson/twin.examples/blob/master/next-emotion-typescript/pages/_document.tsx
+
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { cache } from '@emotion/css'
+import { CacheProvider } from '@emotion/react'
+import GlobalStyles from '@/styles/global-styles'
 
 if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
   require('../mocks')
 }
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <CacheProvider value={cache}>
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </CacheProvider>
+)
+
+export default App
