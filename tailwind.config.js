bfea9d5b6054608d25514a40655866a66a7a4121
p-iknow <apricotsoul@gmail.com>
2023-01-16 00:36:45 +0900

    doc: rerfer 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ const twinConfig = {
       },
     },
   },
+  // https://github.com/daisyui/react-daisyui
   content: ['node_modules/daisyui/dist/**/*.js'],
+  // refer:  https://daisyui.com/docs/install/
   plugins: [require('daisyui')],
 }
 
