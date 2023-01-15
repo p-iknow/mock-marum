cbc061dfab7adef5fe08be184cdeaf1b19ccc0a0
p-iknow <apricotsoul@gmail.com>
2023-01-15 15:30:58 +0900

    config: code style

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: ['prettier'],
+  extends: ['next/core-web-vitals', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+  },
+}
---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "$schema": "http://json.schemastore.org/prettierrc",
+  "arrowParens": "avoid",
+  "bracketSpacing": true,
+  "bracketSameLine": false,
+  "jsxSingleQuote": false,
+  "printWidth": 90,
+  "proseWrap": "always",
+  "quoteProps": "as-needed",
+  "semi": true,
+  "singleQuote": true,
+  "tabWidth": 2,
+  "trailingComma": "es5",
+  "useTabs": false,
+  "semi": false,
+}
